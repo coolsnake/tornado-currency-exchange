9873af79a56a7f1d3619b94a3817537faca7c1e2
lemki <lwwwwwww0@gmail.com>
2017-11-16 00:36:31 +0200

    add tornado app

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+
+import tornado.web
+import tornado.httpserver
+import tornado.gen
+import tornado.ioloop
+import tornado.options
+import tornadoredis
+from tornado.options import define, options
+
+from handlers import MainHandler
+
+
+define('port', default=8888, type=int, help="Run on the given port")
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+        self.db = tornadoredis.Client()
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
