a2b66df0aa2fdc4a2649bd1689fe9cf3d1a93036
lemki <lwwwwwww0@gmail.com>
2017-11-16 04:53:56 +0200

    in models: add id, rename price

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-import datetime
+from datetime import datetime
+import uuid
 
 
 class OrderBook:
-    def __init__(self, status=0, price_per_item=.0, count=.0, order_type='sell'):
+    def __init__(self, status=1, price_per_item=.0, count=.0, order_type='sell'):
         self.status = status  # 0 - delete, 1 - order, 2 - deal
         self.price_per_item = price_per_item
         self.count = count
@@ -10,6 +11,7 @@ class OrderBook:
 
         self.total_price = price_per_item * count
         self.date_time = datetime.now()
+        self.id = uuid.uuid4().hex
 
     def __repr__(self):
         fields = vars(self)
