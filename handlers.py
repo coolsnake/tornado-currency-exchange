e36e8b73f173fe88ce430ba5874e9c21625be983
lemki <lwwwwwww0@gmail.com>
2017-11-17 08:17:04 +0200

    reformatting templates; logic in a separate file

--- handlers.py
+++ handlers.py
@@ -1,10 +1,10 @@
 import tornado.web
 import tornado.gen
-from tornado.escape import json_decode
-from tornado import httpclient
 import json
 
 from models import OrderBook
+
+from utils import get_rate, find_order, deal
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -16,30 +16,23 @@ class BaseHandler(tornado.web.RequestHandler):
 
 class MainHandler(BaseHandler):
     async def get(self):
-        rate = await tornado.gen.Task(self.db.get, 'rate')
-        if rate:
-            self.render("order_form.html", current_bitcoin_rate=rate)
+        rate = await get_rate(self.db)
+
+        orders = await tornado.gen.Task(self.db.get, 'orders')
+        if not orders:
+            orders = list()
         else:
-            http_client = httpclient.AsyncHTTPClient()
-            try:
-                api_response = await http_client.fetch("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
-                decoded = json_decode(api_response.body)
-                rate = decoded['bpi']['USD']['rate_float']
-            except httpclient.HTTPError as e:
-                print("Error: " + str(e))
-            except Exception as e:
-                print("Error: " + str(e))
-            http_client.close()
-            if not rate:
-                raise tornado.web.HTTPError(404)
-            await tornado.gen.Task(self.db.set, 'rate', rate, 60)
-            self.render("order_form.html", current_bitcoin_rate=rate)
+            orders = json.loads(orders)
+
+        self.render("order_form.html", current_bitcoin_rate=rate, orders=orders)
 
     async def post(self, *args, **kwargs):
         order_type = 'buy' if 'buy' in self.request.arguments else 'sell'
         price = float(self.get_argument('price', .0))
         count = float(self.get_argument('count', .0))
         current_order = OrderBook(status=1, price_per_item=price, count=count, order_type=order_type)
+
+        rate = await get_rate(self.db)
 
         orders = await tornado.gen.Task(self.db.get, 'orders')
         if not orders:
@@ -49,37 +42,6 @@ class MainHandler(BaseHandler):
         orders.append(current_order.__dict__)
         await tornado.gen.Task(self.db.set, 'orders', json.dumps(orders))
 
-        #self.find_order(current_order)
+        #find_order(current_order)
 
-        self.render("order_book.html", orders=orders)
-
-    async def find_order(self):
-        pass
-        # искать подходящие заказы с другим типом рекурсивно
-        # если заказ с типом продажа:
-        #   идем по всем заказам на покупку от самого дорогого
-        #   если самый дорогой не подходит, КОНЕЦ
-        #   иначе deal() и find_order()
-        # если заказ с типом покупка:
-        #   идем по всем продажам от самой дешевой
-        #   если самая дешевая не подходит, КОНЕЦ
-        #   иначе deal() и find_order()
-
-    async def deal(self, buy, sell):
-        if buy['count'] == sell['count']:
-            buy['status'] = 2  # change status Order -> Deal
-            sell['status'] = 2
-        elif buy['count'] > sell['count']:
-            sell['status'] = 2  # change status Order -> Deal
-            # create deal copy
-            OrderBook(status=2, price_per_item=sell['price_per_item'],
-                      count=sell['count'], order_type='buy')
-            buy['count'] -= sell['count']  # calculate the remainder
-            buy.update_total_price()
-        elif buy['count'] < sell['count']:
-            buy['status'] = 2
-            OrderBook(status=2, price_per_item=buy['price_per_item'],
-                      count=buy['count'], order_type='sell')
-            sell['count'] -= buy['count']
-            buy.update_total_price()
-        await buy, sell,
+        self.render("order_form.html", current_bitcoin_rate=rate, orders=orders)
